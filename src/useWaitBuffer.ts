1caa4964055e9695ab98feb66064de6ea3271f27
skt-t1-byungi <tiniwz@gmail.com>
2019-07-01 09:26:03 +0900

    fix imporve useWaitBuffer

--- src/useWaitBuffer.ts
+++ src/useWaitBuffer.ts
@@ -2,6 +2,10 @@ import { useRef, useEffect } from 'react'
 
 export default function useWaitBuffer<T> (isWaiting: boolean, val: T) {
     const bufRef = useRef(val)
-    useEffect(() => { bufRef.current = val }, [val])
+
+    useEffect(() => {
+        if (!isWaiting) bufRef.current = val
+    }, [val])
+
     return isWaiting ? bufRef.current : val
 }
